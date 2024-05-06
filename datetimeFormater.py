92e919c5466fd79af97599f0b0ad80e0c7af79ae
Paulo Sanguli <paulosanguli@gmail.com>
2024-05-06 21:22:34 +0100

    feact: adding mehtod for encrypt names of videos before save

---
+++ datetimeFormater.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+
+
+print(datetime.utcnow().date())
+print(datetime.utcnow().time())
--- src/handlers/handler_video.py
+++ src/handlers/handler_video.py
@@ -1,5 +1,8 @@
 """save video on folder resources"""
 from src.resources.__path__ import PATH_RESOURCES
+from datetime import datetime
+
+import cv2
 
 
 
@@ -8,10 +11,18 @@ class Handler:
     def save_video(cls, video: any):
         """save video on dir resources"""
         cls.get_info_video(video)
-        
+        print(PATH_RESOURCES)
+        video.save(f"{PATH_RESOURCES}\\{cls.filename}")
         
     @classmethod
     def get_info_video(cls, video: any):
         """get informations of the file"""
-        print(video.filename)
+        time_formated = str(datetime.utcnow().time()).\
+            replace('.', '_').\
+                replace(':', '-')
+        datetime_str = f"{datetime.utcnow().date()}_{time_formated}"
+        
+        cls.filename = f"{datetime_str}.mp4"
+
+        print(cls.filename)
         
